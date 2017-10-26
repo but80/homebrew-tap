9180854ee2b1803cd9aeacd5da30d138c770efdb
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-26 17:21:48 +0900

    smaf825 version v1.0.1

---
+++ smaf825.rb
@@ -0,0 +1,15 @@
+class Smaf825 < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/but80/smaf825/releases/download/v1.0.1/smaf825_1.0.1_darwin_amd64.tar.gz"
+  version "1.0.1"
+  sha256 "7ac6564eb69c72607a10118ec27dc47acc15c6070ce22c127aaf3bc257205a35"
+
+  def install
+    bin.install "smaf825"
+  end
+
+  test do
+    
+  end
+end
