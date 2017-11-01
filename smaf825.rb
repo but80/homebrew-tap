6117a776605a2797507d06704fd12d4c76f8b80a
goreleaserbot <goreleaser@carlosbecker.com>
2017-11-02 06:52:04 +0900

    smaf825 version v1.2.0

--- smaf825.rb
+++ smaf825.rb
@@ -1,9 +1,9 @@
 class Smaf825 < Formula
   desc ""
   homepage ""
-  url "https://github.com/but80/smaf825/releases/download/v1.1.0/smaf825_1.1.0_darwin_amd64.tar.gz"
-  version "1.1.0"
-  sha256 "6f001d13944b4d1fd8de85cdc2e5d46350756c83f4d056eb2a0bdb1f9b46d81b"
+  url "https://github.com/but80/smaf825/releases/download/v1.2.0/smaf825_1.2.0_darwin_amd64.tar.gz"
+  version "1.2.0"
+  sha256 "139f5e5f5238e2230bc72de6c5c62425e5f142390bb6132061ee72d862c6bb70"
 
   def install
     bin.install "smaf825"
