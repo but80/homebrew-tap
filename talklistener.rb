d14878b7bde588545bb3db3c85806f6a8a0a3398
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-04 14:25:10 +0900

    Brew formula update for talklistener version v0.3.0

--- talklistener.rb
+++ talklistener.rb
@@ -1,9 +1,9 @@
 class Talklistener < Formula
   desc ""
   homepage ""
-  url "https://github.com/but80/talklistener/releases/download/v0.2.0/talklistener_0.2.0_darwin_amd64.tar.gz"
-  version "0.2.0"
-  sha256 "8fdfd4aa71441505144e2c9d0e394a54cbcbb77e1f6f238d0ecd0a897fb8b302"
+  url "https://github.com/but80/talklistener/releases/download/v0.3.0/talklistener_0.3.0_darwin_amd64.tar.gz"
+  version "0.3.0"
+  sha256 "d637264b45056e2ec1321584dcb72964542dc68f8e55f1263418f7f1b9275728"
 
   def install
     bin.install "talklistener"
